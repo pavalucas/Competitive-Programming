d3704e43fdaa254384c996c24693a4b0fd0f98f5
pavalucas <lucasapava@gmail.com>
2019-01-21 23:49:32 -0200

    Added problems from Brazilian ICPC Summer School 2019 - Day 1

---
+++ Problems/Brazilian_ICPC_Summer_School_2019/day1/e.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+#define mp make_pair
+#define mt make_tuple
+#define fi first
+#define se second
+#define pb push_back
+#define sz(x) int((x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define rp(i, n) for (int i = 0; i < (int)(n); ++i)
+#define rep(i, x, n) for (int i = (x); i < (int)(n); ++i)
+#define for1(i, n) for (int i = 1; i <= (int)(n); ++i)
+#define ford(i, n) for (int i = (int)(n) - 1; i >= 0; --i)
+#define fore(i, a, b) for (int i = (int)(a); i < (int)(b); ++i)
+
+#define TRACE(x...)
+#define WATCH(x) TRACE( cout << #x" = " << x << endl)
+#define PRINT(x) TRACE(printf(x))
+#define WATCHR(a, b) TRACE( for(auto c = a; c != b;) cout << *(c++) << " "; cout << endl)
+#define WATCHC(V) TRACE({cout << #V" = "; WATCHR(V.begin(), V.end()); } )
+
+using namespace std;
+
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<pii> vpi;
+typedef vector<vi> vvi;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+
+const int MAXN = 10010;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int t;
+    cin >> t;
+    while(t--) {
+        int n, m;
+        cin >> n >> m;
+        vi vecA(n);
+        vi vecB(m);
+        rp(i,n) cin >> vecA[i];
+        rp(i,m) cin >> vecB[i];
+        sort(all(vecB));
+        int resp = 0;
+        rp(i,n) {
+            int qnt = lower_bound(all(vecB), vecA[i]) - vecB.begin();
+            WATCH(qnt);
+            resp += qnt;
+        }
+        cout << resp << '\n';
+    }
+    return 0;
+}
